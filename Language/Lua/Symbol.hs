9a44b9f06f2f6c2831dde60968343781c9378069
Yohsuke Yukishita <yukishita-y@klab.com>
2013-08-14 21:42:07 +0900

    [SYMBOL] Fix operator precedence (and forgotten '^' :<)

--- Language/Lua/Symbol.hs
+++ Language/Lua/Symbol.hs
@@ -3,19 +3,22 @@ module Language.Lua.Symbol where
 data Intrinsic = Intrinsic String
   deriving(Eq, Ord, Show)
 
+-- http://www.lua.org/manual/5.2/manual.html 3.4.7
 binops :: [Either Intrinsic Intrinsic]
 binops = 
- [Left (Intrinsic ".."), Left (Intrinsic "or"), Left (Intrinsic "and"),
-  Left (Intrinsic "<="), Left (Intrinsic "<"), Left (Intrinsic ">="),
-  Left (Intrinsic ">"), Left (Intrinsic "=="), Left (Intrinsic "~="),
-  Left (Intrinsic "+"), Left (Intrinsic "-"), Left (Intrinsic "/"),
-  Left (Intrinsic "%"), Left (Intrinsic "*")]
+ [Left (Intrinsic "or"), Left (Intrinsic "and"),
+  Left (Intrinsic "<"), Left (Intrinsic ">"), Left (Intrinsic "<="), Left (Intrinsic ">="),
+  Left (Intrinsic "~="),Left (Intrinsic "=="),
+  Right (Intrinsic ".."),
+  Left (Intrinsic "+"), Left (Intrinsic "-"), Left (Intrinsic "*"),
+  Left (Intrinsic "/"), Left (Intrinsic "%"),
+  Right (Intrinsic "^")]
 
 unops :: [Intrinsic]
 unops = [Intrinsic "-", Intrinsic "not", Intrinsic "#"]
 
 stringOps = ["and","or","not"]
 
-reservedWords = 
+reservedWords =
    ["and","break","do","else","elseif","end","false","for","function","if",
     "in","local","nil","not","or", "repeat","return","then","true","until","while"]
